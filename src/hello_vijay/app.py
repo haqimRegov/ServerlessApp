46ade07806b144e8531a6299f941c598b76138ce
haqimRegov <119914583+haqimRegov@users.noreply.github.com>
2023-11-16 11:09:54 +0800

    Update app.py

--- src/hello_vijay/app.py
+++ src/hello_vijay/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template, url_for, flash, redirect
 import json
+import logging
 from ast import literal_eval
 from locmod import update_user_info,getUserDataBmi,calculateBMI,inserBmiData,getDataBmiTable
 from flask_cors import CORS, cross_origin
@@ -13,6 +14,14 @@ def hello_vijay():
     print("Endpoint accessible")
 
     return "Hello Vijay Welcome to Serverless"
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr  # Get the user's IP address
+    logging.info(f'User with IP address {user_ip} visited the website.')
+    
+    # Your other web application code here
+    return 'Hello, World!
 
 
 if __name__ == "__main__":
